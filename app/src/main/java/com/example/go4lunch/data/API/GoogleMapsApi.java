384326a6799765670650694fe4f4e81020381975
Émilie P <emiliep.dev@gmail.com>
2023-04-25 15:20:01 +0200

    Finished RestaurantDetail xml

--- app/src/main/java/com/example/go4lunch/data/API/GoogleMapsApi.java
+++ app/src/main/java/com/example/go4lunch/data/API/GoogleMapsApi.java
@@ -1,5 +1,8 @@
 package com.example.go4lunch.data.API;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.example.go4lunch.data.API.nearby_search_restaurants.NearbySearchResponse;
 
 import retrofit2.Call;
@@ -9,10 +12,10 @@ import retrofit2.http.Query;
 public interface GoogleMapsApi {
     @GET("nearbysearch/json")
     Call<NearbySearchResponse> getNearby(
-        @Query("location") String location,
-        @Query("type") String type,
-        @Query("keyword") String keyword,
-        @Query("rankby") String rankby,
-        @Query("key") String key);
+        @Query("location") @NonNull  String location,
+        @Query("type") @NonNull String type,
+        @Query("keyword") @Nullable String keyword,
+        @Query("rankby") @NonNull String rankby,
+        @Query("key") @NonNull String key);
 
 }
--- app/src/main/java/com/example/go4lunch/data/API/nearby_search_restaurants/NearbySearchRepository.java
+++ app/src/main/java/com/example/go4lunch/data/API/nearby_search_restaurants/NearbySearchRepository.java
@@ -7,9 +7,10 @@ import androidx.lifecycle.MutableLiveData;
 
 import com.example.go4lunch.data.API.GoogleMapsApi;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -17,9 +18,12 @@ import javax.inject.Singleton;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
 @Singleton
 public class NearbySearchRepository {
     private final GoogleMapsApi googleMapsApi;
+
+    private final Map<Integer, List<NearbySearchResult>> nearbySearchCache = new HashMap<>();
 
     @Inject
     public NearbySearchRepository(GoogleMapsApi googleMapsApi) {
@@ -35,26 +39,51 @@ public class NearbySearchRepository {
         @NonNull String key) {
 
         MutableLiveData<NearbySearchWrapper> resultMutableLiveData = new MutableLiveData<>();
-        resultMutableLiveData.setValue(new NearbySearchWrapper.Loading());
+        Integer cacheKey = generateCacheKey(location, type, keyword, rankby, key);
 
-        googleMapsApi.getNearby(location, type, keyword, rankby, key).enqueue(new Callback<NearbySearchResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<NearbySearchResponse> call, @NonNull Response<NearbySearchResponse> response) {
-                if (response.isSuccessful()) {
-                    List<NearbySearchResult> nearbySearchResults = NearbySearchResult.fromNearbySearchResponse(response.body());
-                    resultMutableLiveData.setValue(new NearbySearchWrapper.Success(nearbySearchResults));
-                } else {
-                    resultMutableLiveData.setValue(new NearbySearchWrapper.Error(new Throwable("API call failed")));
-                }
-            }
+        if (!nearbySearchCache.containsKey(cacheKey)) {
 
-            @Override
-            public void onFailure(@NonNull Call<NearbySearchResponse> call, @NonNull Throwable t) {
-                resultMutableLiveData.setValue(new NearbySearchWrapper.Error(t));
-            }
-        });
+            resultMutableLiveData.setValue(new NearbySearchWrapper.Loading());
+            googleMapsApi.getNearby(location, type, keyword, rankby, key).enqueue(
+                new Callback<NearbySearchResponse>() {
+                    @Override
+                    public void onResponse(@NonNull Call<NearbySearchResponse> call, @NonNull Response<NearbySearchResponse> response) {
+                        if (response.isSuccessful()) {
+                            List<NearbySearchResult> nearbySearchResults = NearbySearchResult.fromNearbySearchResponse(response.body());
+                            if (!nearbySearchResults.isEmpty()) {
+                                nearbySearchCache.put(cacheKey, nearbySearchResults);
+                            }
+                            resultMutableLiveData.setValue(new NearbySearchWrapper.Success(nearbySearchResults));
+                        } else {
+                            resultMutableLiveData.setValue(new NearbySearchWrapper.Error(new Throwable("API call failed")));
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(@NonNull Call<NearbySearchResponse> call, @NonNull Throwable t) {
+                        resultMutableLiveData.setValue(new NearbySearchWrapper.Error(t));
+                    }
+                });
+        } else   resultMutableLiveData.setValue(new NearbySearchWrapper.Success(nearbySearchCache.get(cacheKey)));
         return resultMutableLiveData;
-    }
 
     }
+
+    private Integer generateCacheKey(
+        @NonNull String location,
+        @NonNull String type,
+        @Nullable String keyword,
+        @NonNull String rankby,
+        @NonNull String key
+    ) {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + location.hashCode();
+        result = prime * result + type.hashCode();
+        result = prime * result + (keyword != null ? keyword.hashCode() : 0); // because Nullable
+        result = prime * result + rankby.hashCode();
+        result = prime * result + key.hashCode();
+        return result;
+    }
+
+}
