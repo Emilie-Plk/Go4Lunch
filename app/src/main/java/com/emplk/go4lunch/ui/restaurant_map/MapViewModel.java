6d4b047a19b72453632065190de44d9f0d0580d9
Émilie P <emiliep.dev@gmail.com>
2023-05-23 15:11:05 +0200

    MapsFragment MVVMized(ish)

--- app/src/main/java/com/emplk/go4lunch/ui/restaurant_map/MapViewModel.java
+++ app/src/main/java/com/emplk/go4lunch/ui/restaurant_map/MapViewModel.java
@@ -1,9 +1,15 @@
 package com.emplk.go4lunch.ui.restaurant_map;
 
+import android.location.Location;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.emplk.go4lunch.data.gps_location.GPSLocationRepositoryImpl;
+import com.google.android.gms.maps.model.LatLng;
 
 import javax.inject.Inject;
 
@@ -14,17 +20,32 @@ public class MapViewModel extends ViewModel {
 
     private final GPSLocationRepositoryImpl gpsLocationRepository;
 
+    private final MediatorLiveData<MapViewState> mapViewStateMediatorLiveData = new MediatorLiveData<>();
+
+
     @Inject
     public MapViewModel(
         @NonNull GPSLocationRepositoryImpl gpsLocationRepository
     ) {
         this.gpsLocationRepository = gpsLocationRepository;
+
+        mapViewStateMediatorLiveData.addSource(gpsLocationRepository.getLocationLiveData(), location -> {
+                combine(location);
+            }
+        );
     }
 
-    /*   public LiveData<MapViewState> getLocationLiveData() {
-     return Transformations.switchMap(gpsLocationRepository.getLocationLiveData(), location -> {
-
+    private void combine(@Nullable Location location) {
+        if (location == null) {
+            return;
         }
-        );
-    }*/
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MapViewState mapViewState = new MapViewState(latLng);
+        mapViewStateMediatorLiveData.setValue(mapViewState);
+    }
+
+    public LiveData<MapViewState> getLocationLiveData() {
+        return mapViewStateMediatorLiveData;
+    }
 }
